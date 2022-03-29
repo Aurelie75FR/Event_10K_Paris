8affa2005176220f22493f497759df4927d25729
Aurelie75FR <aurelielo.vi@gmail.com>
2022-03-29 20:47:47 +0200

    Config

---
+++ back/model/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: { type: String, unique: true, required: true },
+    photo: String,
+    email: { type: String, unique: true, required: true },
+    phoneNumber: Number,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
